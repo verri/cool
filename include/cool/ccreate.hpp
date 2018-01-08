e6603da56b8143546ebc33371e7495f6a2dc6a8b
Filipe Verri <filipeverri@gmail.com>
2018-01-08 14:28:42 -0200

    ccreate: Added support to stateful deleter.

--- include/cool/ccreate.hpp
+++ include/cool/ccreate.hpp
@@ -3,13 +3,36 @@
 #define COOL_CCREATE_HXX_INCLUDED
 
 #include <memory>
+#include <type_traits>
 
 namespace cool
 {
 
+namespace detail
+{
+#if __cplusplus >= 201703L
+template <typename F, typename... Args> using result_of = std::invoke_result<F, Args...>;
+#else
+template <typename F, typename... Args> using result_of = std::result_of<F(Args...)>;
+#endif
+}
+
 template <typename T, typename R> auto ccreate(T* ptr, R (*deleter)(T*)) -> std::unique_ptr<T, R (*)(T*)>
 {
   return {ptr, deleter};
+}
+
+template <typename T, typename F, typename R = typename detail::result_of<F, T*>::type>
+auto ccreate(T* ptr, F deleter) ->
+  typename std::enable_if<std::is_convertible<F, R (*)(T*)>::value, std::unique_ptr<T, R (*)(T*)>>::type
+{
+  return {ptr, deleter};
+}
+
+template <typename T, typename F, typename R = typename detail::result_of<F, T*>::type>
+auto ccreate(T* ptr, F deleter) -> typename std::enable_if<!std::is_convertible<F, R (*)(T*)>::value, std::unique_ptr<T, F>>::type
+{
+  return {ptr, std::move(deleter)};
 }
 
 } // namespace cool
--- test/ccreate.cpp
+++ test/ccreate.cpp
@@ -3,12 +3,24 @@
 #include <catch.hpp>
 
 #include <cstdio>
+#include <cstdlib>
 
 TEST_CASE("Basic ccreate functionality", "[ccreate]")
 {
   auto fp1 = cool::ccreate(std::tmpfile(), std::fclose);
-  auto fp2 = cool::ccreate(std::tmpfile(), +[](std::FILE* fp) {
+  auto fp2 = cool::ccreate(std::tmpfile(), [](std::FILE* fp) {
     std::fflush(fp);
     std::fclose(fp);
   });
+
+  auto p = cool::ccreate(std::malloc(1), std::free);
+
+  int i = 0;
+  {
+    cool::ccreate(std::malloc(1), [&i](void* p) {
+      std::free(p);
+      i = 1;
+    });
+  }
+  CHECK(i == 1);
 }
