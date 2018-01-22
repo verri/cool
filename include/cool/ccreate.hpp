a5bee64f94f3cd95d27045235b53720ecb57e2ba
Filipe Verri <filipeverri@gmail.com>
2018-01-22 17:23:31 -0200

    ccreate: result_of as a macro.
    * result_of is also used in thread_pool, so we could have problems
      in the future if both libraries are used together.

--- include/cool/ccreate.hpp
+++ include/cool/ccreate.hpp
@@ -5,31 +5,28 @@
 #include <memory>
 #include <type_traits>
 
+#if __cplusplus >= 201703L
+#define RESULT_OF_T(F, ...) std::invoke_result_t<F, __VA_ARGS__>
+#else
+#define RESULT_OF_T(F, ...) typename std::result_of<F(__VA_ARGS__)>::type
+#endif
+
 namespace cool
 {
-
-namespace detail
-{
-#if __cplusplus >= 201703L
-template <typename F, typename... Args> using result_of = std::invoke_result<F, Args...>;
-#else
-template <typename F, typename... Args> using result_of = std::result_of<F(Args...)>;
-#endif
-}
 
 template <typename T, typename R> constexpr auto ccreate(T* ptr, R (*deleter)(T*)) noexcept -> std::unique_ptr<T, R (*)(T*)>
 {
   return {ptr, deleter};
 }
 
-template <typename T, typename F, typename R = typename detail::result_of<F, T*>::type>
+template <typename T, typename F, typename R = RESULT_OF_T(F, T*)>
 constexpr auto ccreate(T* ptr, F deleter) noexcept ->
   typename std::enable_if<std::is_convertible<F, R (*)(T*)>::value, std::unique_ptr<T, R (*)(T*)>>::type
 {
   return {ptr, deleter};
 }
 
-template <typename T, typename F, typename R = typename detail::result_of<F, T*>::type>
+template <typename T, typename F, typename R = RESULT_OF_T(F, T*)>
 constexpr auto ccreate(T* ptr, F deleter) noexcept(std::is_nothrow_move_constructible<F>::value) ->
   typename std::enable_if<!std::is_convertible<F, R (*)(T*)>::value, std::unique_ptr<T, F>>::type
 {
@@ -37,5 +34,7 @@ constexpr auto ccreate(T* ptr, F deleter) noexcept(std::is_nothrow_move_construc
 }
 
 } // namespace cool
+
+#undef RESULT_OF_T
 
 #endif // COOL_CCREATE_HXX_INCLUDED
