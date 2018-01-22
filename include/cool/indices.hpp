dc5483d3d3b21fb3943a41104f607bfd66592d3f
Filipe Verri <filipeverri@gmail.com>
2018-01-22 16:56:35 -0200

    indices: Added constexp and noexcept annotations.
    * Fixes #1

--- include/cool/indices.hpp
+++ include/cool/indices.hpp
@@ -4,6 +4,12 @@
 #include <iterator>
 #include <type_traits>
 
+#if __cpp_constexpr >= 201304
+#define RELAXED_CONSTEXPR constexpr
+#else
+#define RELAXED_CONSTEXPR
+#endif
+
 namespace cool
 {
 
@@ -18,41 +24,43 @@ public:
   using difference_type = std::ptrdiff_t;
   using iterator_category = std::bidirectional_iterator_tag;
 
-  index_iterator() = default;
-  explicit index_iterator(T value) : value_{std::move(value)} {}
+  constexpr index_iterator() noexcept = default;
 
-  auto operator++() -> index_iterator&
+  constexpr explicit index_iterator(T value) noexcept : value_{std::move(value)} {}
+
+  RELAXED_CONSTEXPR auto operator++() noexcept -> index_iterator&
   {
     ++value_;
     return *this;
   }
 
-  auto operator++(int) -> index_iterator
+  RELAXED_CONSTEXPR auto operator++(int)noexcept -> index_iterator
   {
     auto copy = *this;
     ++(*this);
     return copy;
   }
 
-  auto operator--() -> index_iterator&
+  RELAXED_CONSTEXPR auto operator--() noexcept -> index_iterator&
   {
     --value_;
     return *this;
   }
 
-  auto operator--(int) -> index_iterator
+  RELAXED_CONSTEXPR auto operator--(int)noexcept -> index_iterator
   {
     auto copy = *this;
     --(*this);
     return copy;
   }
 
-  auto operator*() -> reference { return value_; }
+  RELAXED_CONSTEXPR auto operator*() noexcept -> reference { return value_; }
 
-  auto operator-(const index_iterator& other) const -> difference_type { return value_ - other.value_; }
+  constexpr auto operator-(const index_iterator& other) const noexcept -> difference_type { return value_ - other.value_; }
 
-  auto operator==(const index_iterator& other) const -> bool { return value_ == other.value_; }
-  auto operator!=(const index_iterator& other) const -> bool { return value_ != other.value_; }
+  constexpr auto operator==(const index_iterator& other) const noexcept -> bool { return value_ == other.value_; }
+
+  constexpr auto operator!=(const index_iterator& other) const noexcept -> bool { return value_ != other.value_; }
 
 private:
   T value_;
@@ -64,29 +72,33 @@ public:
   using value_type = T;
   using size_type = std::size_t;
 
-  index_range(T begin, T end) : begin_{std::move(begin)}, end_{std::move(end)} {}
+  constexpr index_range(T begin, T end) noexcept : begin_{std::move(begin)}, end_{std::move(end)} {}
 
-  auto begin() const -> index_iterator<T> { return begin_; }
-  auto end() const -> index_iterator<T> { return end_; }
+  constexpr auto begin() const noexcept -> index_iterator<T> { return begin_; }
+  constexpr auto end() const noexcept -> index_iterator<T> { return end_; }
 
-  auto size() const -> size_type { return end_ - begin_; }
+  constexpr auto size() const noexcept -> size_type { return end_ - begin_; }
 
 private:
   index_iterator<T> begin_, end_;
 };
 
-template <typename T> auto indices(T value) -> typename std::enable_if<std::is_integral<T>::value, index_range<T>>::type
+template <typename T>
+constexpr auto indices(T value) noexcept -> typename std::enable_if<std::is_integral<T>::value, index_range<T>>::type
 {
   return {T{}, std::move(value)};
 }
 
 template <typename T, typename U>
-auto indices(T begin, U end) -> typename std::enable_if<std::is_integral<T>::value && std::is_integral<U>::value,
-                                                        index_range<typename std::common_type<T, U>::type>>::type
+constexpr auto indices(T begin, U end) noexcept ->
+  typename std::enable_if<std::is_integral<T>::value && std::is_integral<U>::value,
+                          index_range<typename std::common_type<T, U>::type>>::type
 {
   return {std::move(begin), std::move(end)};
 }
 
 } // namespace cool
+
+#undef RELAXED_CONSTEXPR
 
 #endif // COOL_INDICES_HPP_INCLUDED
