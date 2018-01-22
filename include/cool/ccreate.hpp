e3360fedf3baf6ebe462264287846743e45e82fe
Filipe Verri <filipeverri@gmail.com>
2018-01-22 16:43:49 -0200

    ccreate: noexcept and constexpr.

--- include/cool/ccreate.hpp
+++ include/cool/ccreate.hpp
@@ -17,20 +17,21 @@ template <typename F, typename... Args> using result_of = std::result_of<F(Args.
 #endif
 }
 
-template <typename T, typename R> auto ccreate(T* ptr, R (*deleter)(T*)) -> std::unique_ptr<T, R (*)(T*)>
+template <typename T, typename R> constexpr auto ccreate(T* ptr, R (*deleter)(T*)) noexcept -> std::unique_ptr<T, R (*)(T*)>
 {
   return {ptr, deleter};
 }
 
 template <typename T, typename F, typename R = typename detail::result_of<F, T*>::type>
-auto ccreate(T* ptr, F deleter) ->
+constexpr auto ccreate(T* ptr, F deleter) noexcept ->
   typename std::enable_if<std::is_convertible<F, R (*)(T*)>::value, std::unique_ptr<T, R (*)(T*)>>::type
 {
   return {ptr, deleter};
 }
 
 template <typename T, typename F, typename R = typename detail::result_of<F, T*>::type>
-auto ccreate(T* ptr, F deleter) -> typename std::enable_if<!std::is_convertible<F, R (*)(T*)>::value, std::unique_ptr<T, F>>::type
+constexpr auto ccreate(T* ptr, F deleter) noexcept(std::is_nothrow_move_constructible<F>::value) ->
+  typename std::enable_if<!std::is_convertible<F, R (*)(T*)>::value, std::unique_ptr<T, F>>::type
 {
   return {ptr, std::move(deleter)};
 }
