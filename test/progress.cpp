454ee9997df40b2b9cb6da9d034e4d89271446ce
Filipe Verri <filipeverri@gmail.com>
2018-04-05 10:15:22 -0300

    progress: Added test case.

--- test/progress.cpp
+++ test/progress.cpp
@@ -4,14 +4,23 @@
 
 TEST_CASE("Basic progress functionality", "[progress]")
 {
-  cool::progress<> pb(0.01, std::chrono::seconds{0});
+  {
+    cool::progress<> pb(0.01, std::chrono::seconds{0});
 
-  const auto ok = pb.update(0.1, [](double diff, cool::progress<>::duration elapsed) {
-    CHECK(diff == Approx(0.1));
-    CHECK(elapsed > cool::progress<>::duration(0));
-  });
+    CHECK(pb.update(0.1, [](double diff, cool::progress<>::duration elapsed) {
+      CHECK(diff == Approx(0.1));
+      CHECK(elapsed > cool::progress<>::duration(0));
+    }));
 
-  CHECK(ok);
+    CHECK_FALSE(pb.update(0.001, [](double, cool::progress<>::duration) {}));
+  }
 
-  CHECK_FALSE(pb.update(0.001, [](double, cool::progress<>::duration) {}));
+  {
+    cool::progress<> pb(0, std::chrono::hours{1});
+
+    CHECK_FALSE(pb.update(1, [](double diff, cool::progress<>::duration elapsed) {
+      CHECK(diff == Approx(1.0));
+      CHECK(elapsed < std::chrono::hours{1});
+    }));
+  }
 }
